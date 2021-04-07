4db01c59fa695c35ba99319ff74be6888588f39b
Norman Schultz <>
2021-04-07 15:43:31 -0600

    Update request_helper.rb

--- lib/request_helper.rb
+++ lib/request_helper.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
+require 'dotenv'
 require 'net/http'
 require 'uri'
+Dotenv.load('token.env')
 
 module RequestHelper
   def request(method, path, options = {}, url = BASE_URL)
@@ -24,6 +26,19 @@ module RequestHelper
       http.request(request)
     end
     puts "#{stock} #{url.path} returned #{response.code}"
+    JSON.parse(response.body)
+  end
+
+  def call_fmp(path, stock)
+    site = 'https://financialmodelingprep.com/api/v3'
+    uri = URI.parse(site + path + stock + "?apikey=#{ENV['TOKEN_FMP']}")
+    request = Net::HTTP::Get.new(uri)
+    request['Upgrade-Insecure-Requests'] = '1'
+    req_options = { use_ssl: uri.scheme == 'https' }
+    response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+      http.request(request)
+    end
+    puts "#{stock} #{path} returned #{response.code}"
     JSON.parse(response.body)
   end
 end
