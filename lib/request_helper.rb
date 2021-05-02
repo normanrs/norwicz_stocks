5a49c5e2b7e43c97d799909305df8fadfeb362f3
normanrs <normanrs@live.com>
2021-05-02 09:03:03 -0600

    Delete unused code

--- lib/request_helper.rb
+++ lib/request_helper.rb
@@ -6,17 +6,6 @@ require 'uri'
 Dotenv.load('token.env')
 
 module RequestHelper
-  def request(method, path, options = {}, url = BASE_URL)
-    # Builds a request with parameters and returns the last response
-    build_request(method, url, path)
-    unless options.empty?
-      add_params(options.fetch(:params, {}),
-                 options.fetch(:headers, {}),
-                 options)
-    end
-    respond
-  end
-
   def api_call(uri)
     request = Net::HTTP::Get.new(uri)
     request['Upgrade-Insecure-Requests'] = '1'
