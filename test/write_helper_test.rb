3e07c321932d7a85002c724eaaa3303f8fcc7834
normanrs <normanrs@live.com>
2021-05-02 12:35:16 -0600

    Add write helper

---
+++ test/write_helper_test.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require './test/test_helper.rb'
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/write_helper.rb'
+
+class WriteHelperTest < Minitest::Test
+  include WriteHelper
+
+  def setup; end
+
+  def test_write_json
+    test_method = write_json('tmp/write_helper.json', sample_fmp_data)
+    assert_instance_of Integer, test_method
+  end
+
+end
