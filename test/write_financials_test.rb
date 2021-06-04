02f0ae8448133cbef72cbc4c2968d7c0ae81c017
normanrs <normanrs@live.com>
2021-06-04 12:05:21 -0600

    Update write_financials_test.rb

--- test/write_financials_test.rb
+++ test/write_financials_test.rb
@@ -14,14 +14,6 @@ class WriteFinancialsTest < Minitest::Test
     assert_instance_of Time, write_time
   end
 
-  def test_merge_hashes
-    hash1 = JSON.parse(File.read('./test/data/stock_data.json'), {})
-    hash2 = JSON.parse(File.read('./test/data/stock_data2.json'), {})
-    merged = WriteFinancials.merge_hashes(hash1, hash2)
-    merged.each { |k, _v| merged[k].merge!(hash2.dig(k)) }
-    assert_equal hash1.count, merged.count
-  end
-
   def test_top_picks
     test_method = WriteFinancials.top_picks
     refute_empty test_method
